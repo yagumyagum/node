779f9a18aee3dd7499ba54fe45550000857c557c
야금야금(YAGUMYAGUM) <manager@ygygcoop.com>
2025-12-09 07:46:04 +0900

    Update upload.sh

--- node16/upload.sh
+++ node16/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker buildx use default
+sh ../buildx.sh
 echo $DOCKER_UPLOAD | docker login ghcr.io -u yagumyagum --password-stdin
 docker buildx build \
   --platform linux/amd64,linux/arm64,linux/arm/v7 \
