c40ce0c06b61a6b5b0da28da3d278a577068f584
야금야금(YAGUMYAGUM) <manager@ygygcoop.com>
2025-12-09 14:33:26 +0900

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,2 +1,42 @@
 #!/bin/sh
-docker buildx use silly_banzai
+DOCKER_BUILDKIT=1
+BUILDKIT_SECURITY_INSECURE=1
+PLATFORM="linux/amd64,linux/arm64"
+
+cd ./node24 && \
+docker buildx build \
+--platform ${PLATFORM} \
+-t ghcr.io/yagumyagum/node24-ygyg:latest \
+--push .
+
+cd ../
+
+cd ./node22 && \
+docker buildx build \
+--platform ${PLATFORM} \
+-t ghcr.io/yagumyagum/node22-ygyg:latest \
+--push .
+
+cd ../
+
+cd ./node20 && \
+docker buildx build \
+--platform ${PLATFORM} \
+-t ghcr.io/yagumyagum/node20-ygyg:latest \
+--push .
+
+cd ../
+
+cd ./node18 && \
+docker buildx build \
+--platform ${PLATFORM} \
+-t ghcr.io/yagumyagum/node18-ygyg:latest \
+--push .
+
+cd ../
+
+cd ./node16 && \
+docker buildx build \
+--platform ${PLATFORM} \
+-t ghcr.io/yagumyagum/node16-ygyg:latest \
+--push .
