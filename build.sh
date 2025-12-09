d85bba1a48b531ab5c166e921a4471340fcf72a6
야금야금(YAGUMYAGUM) <manager@ygygcoop.com>
2025-12-09 09:08:07 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sh ./buildx.sh
 echo $DOCKER_UPLOAD | docker login ghcr.io -u yagumyagum --password-stdin
 
 docker buildx build \
-  --platform linux/amd64,linux/arm64 \
+  --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm64/v8 \
   -t ghcr.io/yagumyagum/node:latest \
   --push .
 
